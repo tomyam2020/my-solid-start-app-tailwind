e2b6b35b49df13c3b3d53b0634cf86a4e83b9d47
tomyam2020 <tomateyam@gmail.com>
2024-09-11 03:45:00 +0900

    [change] solidjsのルールのみeslintで管理するように変更

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,14 @@
-import js from "@eslint/js";
 import solid from "eslint-plugin-solid/configs/typescript";
 import tsParser from "@typescript-eslint/parser";
-import globals from "globals";
 
 export default [
-  js.configs.recommended,
   {
     files: ["**/*.{ts,tsx}"],
     ...solid,
     languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
       parser: tsParser,
       parserOptions: {
         project: "tsconfig.json",
-        ecmaVersion: "latest",
       },
     },
   },
